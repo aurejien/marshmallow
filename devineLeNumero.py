f2d90d48ba13890d3798b8dae360e17a3953a78b
Marshmallow <au8relien@gmail.com>
2020-01-09 12:13:56 +0100

    Damn.
    Holly hop.

--- devineLeNumero.py
+++ devineLeNumero.py
@@ -10,6 +10,7 @@ print("              codeby åurejien               \n ")
 print("----------------------------------------------\n")
 print("Nombre d'essais")
 while x!=0:
+  try:
    if x == 6:
      print("■■■■■■")
    elif x == 5:
@@ -63,6 +64,9 @@ while x!=0:
      print("Plus grand")
    elif n>a:
      print("Plus petit")
+  except ValueError:
+    print("Erreur d'entrée des données")
+    continue
      
      
      
